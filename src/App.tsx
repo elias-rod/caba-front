f6c45a0012422b1f4438a6461e5423a8ee74476d
Elias Rodriguez <elias.rodriguez@southworks.com>
2023-07-11 18:36:45 -0300

    Working

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [serverConnectionStatus, setServerConnectionStatus] = useState('offline');
+  
+  async function ConnectToServer() {
+    setServerConnectionStatus('connecting');
+    try {
+      await fetch("https://localhost:32816/WeatherForecast");
+      setServerConnectionStatus('connected');
+    } catch (error) {
+      setServerConnectionStatus(`error: ${error}`);
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={ConnectToServer}>Connect to server</button>
+        <p>{serverConnectionStatus}</p>
       </header>
     </div>
   );
